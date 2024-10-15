013f4cfa562d0ce9333144f17cf94d36041bf90e
ezzeldien490 <ezzeldien490@gmail.com>
2024-10-15 17:03:38 +0300

    Foor loop

--- Algorithms_And_Problem-solving-Level-1/032-PowerOfM.cpp
+++ Algorithms_And_Problem-solving-Level-1/032-PowerOfM.cpp
@@ -3,25 +3,33 @@
 
 using namespace std;
 
+void ReadInfo(float &Number, unsigned short &PowerValue)
+{
+	cout << "Please enter the number?\n";
+	cin >> Number; cout << endl;
+
+	cout << "Please enter the power value?\n";
+	cin >> PowerValue; cout << endl << endl;
+}
+
 float PowerOfM(float Number, unsigned short PowerValue)
 {
-
-	float Result = pow(Number, PowerValue);
+	if (PowerValue == 0)
+		return 1;
+	float Result = 1;
+	for (short Counter = 0; Counter < PowerValue; Counter++)
+	{
+		 Result*= Number;
+	}
 	return Result;
 }
 int main()
 {
 	float Number;
 	unsigned short PowerValue;
-
-	cout << "Please enter the number?\n";
-	cin >> Number; cout << endl;
-
-	cout << "Please enter the power value?\n";
-	cin >> PowerValue; cout << endl << endl;
-
-	float Result = PowerOfM(Number, PowerValue);
-	cout << Result << "  Round : " << round(Result);
+	
+	ReadInfo(Number, PowerValue);
+	cout << PowerOfM(Number, PowerValue);
 
 	return 0;
 }
