65c511481277289f867f6995e950bbdd7d9eec9f
ezzeldien490 <ezzeldien490@gmail.com>
2024-10-15 19:07:32 +0300

    ,,

--- Cpp_Level-1/Lesson#49-NestedForLoops/H1.cpp
+++ Cpp_Level-1/Lesson#49-NestedForLoops/H1.cpp
@@ -6,10 +6,12 @@ int main()
 {
     for (short i = 65; i <= 90; i++)
     {
+        cout << "letter: " << char(i) << endl;;
         for (short j = 65; j <= 90; j++)
         {
             cout << char(i) << char(j) << endl;
         }
+        cout << "-----------------------------\n";
     }
 
     return 0;
