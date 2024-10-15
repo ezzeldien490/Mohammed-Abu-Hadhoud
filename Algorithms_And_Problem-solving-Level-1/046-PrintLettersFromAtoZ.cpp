caef3cdf6e89484d359d4933e39cf88109377aef
ezzeldien490 <ezzeldien490@gmail.com>
2024-10-15 17:09:26 +0300

    changes

--- Algorithms_And_Problem-solving-Level-1/046-PrintLettersFromAtoZ.cpp
+++ Algorithms_And_Problem-solving-Level-1/046-PrintLettersFromAtoZ.cpp
@@ -5,14 +5,21 @@ using namespace std;
 void PrintUpperCaseLetters()
 {
     for(int i = 65; i <= 90; i++)
-    {
         cout << char(i);
-    }
+    
+}
+
+void PrintLowerCaseLetters()
+{
+    for(int i = 97; i <= 122; i++)
+        cout << char(i);
 }
 
 int main()
 {
     PrintUpperCaseLetters();
+    cout << endl;
+    PrintLowerCaseLetters();
 
     return 0;
 }
