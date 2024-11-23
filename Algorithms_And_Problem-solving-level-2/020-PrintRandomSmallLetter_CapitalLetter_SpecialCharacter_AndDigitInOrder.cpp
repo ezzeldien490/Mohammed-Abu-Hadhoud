aae736a67a5157ebf5b1e2295a7f4cd57ab69ce2
ezzeldien490 <ezzeldien490@gmail.com>
2024-11-23 17:01:49 +0200

    problem 20,21

---
+++ Algorithms_And_Problem-solving-level-2/020-PrintRandomSmallLetter_CapitalLetter_SpecialCharacter_AndDigitInOrder.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+enum enRandomOrder {SmallLetter = 1, CapitalLetter = 2, SpecialCharacter  = 3, Digit = 4};
+
+
+int RandomNumber(int From, int To)
+{
+    return rand() % (To - From + 1) + From;
+}
+
+char GetRandomCharacter(enRandomOrder CharType)
+{
+    switch (CharType)
+    {
+        case SmallLetter:
+            return char(RandomNumber(97, 122));
+        case CapitalLetter:
+            return char(RandomNumber(65, 90));
+        case SpecialCharacter:
+            return char(RandomNumber(33, 47));
+        case Digit:
+            return char(RandomNumber(48, 57));
+    }   
+}
+
+
+int main()
+{
+    srand((unsigned)time(__null));
+
+    cout << GetRandomCharacter(SmallLetter) << endl;
+    cout << GetRandomCharacter(CapitalLetter) << endl;
+    cout << GetRandomCharacter(SpecialCharacter) << endl;
+    cout << GetRandomCharacter(Digit) << endl;
+
+    return 0;
+}
---
+++ Algorithms_And_Problem-solving-level-2/021-GenerateKeys.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+enum enRandomOrder {SmallLetter = 1, CapitalLetter = 2, SpecialCharacter  = 3, Digit = 4};
+
+int ReadPositiveNumber(string Message)
+{
+    int Number;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number; cout << endl;
+    }while(Number <= 0);
+
+    return Number;
+}
+
+int RandomNumber(int From, int To)
+{
+    int RandNumber = rand() % (To - From + 1) + From;
+    return RandNumber;
+}
+
+char GetRandomCharacter(enRandomOrder CharType)
+{
+    switch (CharType)
+    {
+        case SmallLetter:
+            return char(RandomNumber(97, 122));
+        case CapitalLetter:
+            return char(RandomNumber(65, 90));
+        case SpecialCharacter:
+            return char(RandomNumber(33, 47));
+        case Digit:
+            return char(RandomNumber(48, 57));
+    }
+}
+
+string Generateword(enRandomOrder CharType, int Lenght)
+{
+    string Word = "";
+    for (int i = 1; i <= Lenght; i++)
+    {
+        Word += GetRandomCharacter(CharType);
+    }
+
+    return Word;
+}
+
+string GenerateKey()
+{
+    string Key = "";
+
+    Key += Generateword(CapitalLetter, 4) + "-";
+    Key += Generateword(CapitalLetter, 4) + "-";
+    Key += Generateword(CapitalLetter, 4) + "-";
+    Key += Generateword(CapitalLetter, 4);
+
+    return Key;
+}
+
+void GenerateKeys(int NumberOfKeys)
+{
+    for (int i = 1; i <= NumberOfKeys; i++)
+    {
+        cout << "Key [" << i << "]: ";
+        cout << GenerateKey() << endl;
+    }
+}
+
+
+int main()
+{
+    srand((unsigned)time(__null));
+    GenerateKeys(ReadPositiveNumber("Enter Number of keys you want generate?"));
+
+    return 0;
+}
